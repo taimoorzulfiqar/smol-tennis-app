f24a9e450bbad96f471cb403505cb0cde8993233
taimoorzulfiqar <taimoorzulfiqar97@gmail.com>
2025-08-21 08:38:31 -0700

    Fix React build: Move config.js to src directory - Move config.js from root to src/ to comply with React build restrictions - Update import path in GoogleSheetsContext

--- src/context/GoogleSheetsContext.js
+++ src/context/GoogleSheetsContext.js
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import axios from 'axios';
-import config from '../../config.js';
+import config from '../config.js';
 
 const GoogleSheetsContext = createContext();
 
